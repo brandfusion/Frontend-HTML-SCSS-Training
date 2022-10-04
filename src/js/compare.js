3692c78f9a0aa68deb0f6b6f3a7aae4573baee91
iurascualexandru <alexandruiurascu@yahoo.com>
2022-10-04 12:06:10 +0300

    #10 array elemente

--- src/js/compare.js
+++ src/js/compare.js
@@ -1,7 +1,7 @@
 const numberOfElements = 0;
 
-const removeFromCompare = e =>{
-  const {id} = e.currentTarget;
+const removeFromCompare = e => {
+  const { id } = e.currentTarget;
   const target = document.querySelector(`[data-ref="${id}"]`);
   target.remove();
 };
@@ -19,38 +19,50 @@ const insertInCompare = (e) => {
     <h4>${title}</h4>
     </div >
     `;
-    document.querySelector(`.offcanvas-body`).insertAdjacentHTML("beforeend", output);
+  document.querySelector(`.offcanvas-body`).insertAdjacentHTML("beforeend", output);
 };
 
-const checkItemAvailability = ({node}) => {
-  if( node.children.length === 3) {
-    return {availability: false, message: "Nu poate fi adaugat, exista deja 3 elemente in lista"};
-  } 
-    return {availability: true, message: null};  
+const checkItemAvailability = ({ node }) => {
+  if (node.children.length === 3) {
+    return { availability: false, message: "Nu poate fi adaugat, exista deja 3 elemente in lista" };
+  }
+  return { availability: true, message: null };
 };
 
 document.querySelectorAll("[data-role=\"compare-trigger\"]").forEach(el => el.addEventListener("click", e => {
-  const canInsert = checkItemAvailability({node: document.querySelector(`.offcanvas-body`)});
-  const {availability,message} = canInsert;
+  const canInsert = checkItemAvailability({ node: document.querySelector(`.offcanvas-body`) });
+  const { availability, message } = canInsert;
 
-  if(availability) {
-     if(e.currentTarget.checked) {
-      insertInCompare(e);            
-     } else {
-      removeFromCompare(e);  
-     }       
-  } else if(!e.currentTarget.checked) {
-    removeFromCompare(e);  
+  if (availability) {
+    if (e.currentTarget.checked) {
+      insertInCompare(e);
+    } else {
+      removeFromCompare(e);
+    }
+  } else if (!e.currentTarget.checked) {
+    removeFromCompare(e);
   } else {
     e.preventDefault();
     alert(message);
   }
-  
+
 
   // UX 
-  if(document.querySelector(`.offcanvas-body`).children.length>0) {
+  if (document.querySelector(`.offcanvas-body`).children.length > 0) {
     document.querySelector(`.offcanvas-start`).classList.add(`show`);
   } else {
     document.querySelector(`.offcanvas-start`).classList.remove(`show`);
   }
 }));
+
+
+const products = [
+  { image: `resources/images/category-1.png`, title: `Nullam` },
+  { image: `resources/images/category-2.png`, title: `Duis` },
+  { image: `resources/images/category-3.png`, title: `Aliquam` },
+  { image: `resources/images/category-4.png`, title: `Praesent` },
+  { image: `resources/images/category-5.png`, title: `Quisque` },
+  { image: `resources/images/category-6.png`, title: `Etiam` },
+  { image: `resources/images/category-7.png`, title: `Cras` },
+  { image: `resources/images/category-8.png`, title: `Vivamus` }
+];
