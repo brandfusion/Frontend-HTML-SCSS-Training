67e381b1ff707b32b428f3720192770893879cb4
iurascualexandru <alexandruiurascu@yahoo.com>
2022-10-06 09:57:23 +0300

    #10 eliminare si adaugare in compare

--- src/js/compare.js
+++ src/js/compare.js
@@ -1,79 +1,10 @@
 const { Alert } = require("bootstrap");
 
-const numberOfElements = 0;
-
-const removeFromCompare = e => {
-  const { id } = e.currentTarget;
-  const target = document.querySelector(`[data-ref="${id}"]`);
-  target.remove();
-};
-
-const insertInCompare = (e) => {
-  const id = e.currentTarget.getAttribute("id");
-  const img = e.currentTarget.previousElementSibling.src;
-  const title = e.currentTarget.nextElementSibling.textContent;
-  const output = `
-    <div data-ref="${id}" class="compare-item">
-      <div class="d-flex">
-        <img src= "${img}">
-        <button type="button" class="btn-close"> </button>
-      </div >
-    <h4>${title}</h4>
-    </div >
-    `;
-  document.querySelector(`.offcanvas-body`).insertAdjacentHTML("beforeend", output);
-};
-
-const checkItemAvailability = ({ node }) => {
-  if (node.children.length === 3) {
-    return { availability: false, message: "Nu poate fi adaugat, exista deja 3 elemente in lista" };
-  }
-  return { availability: true, message: null };
-};
-
-// document.querySelectorAll("[data-role=\"compare-trigger\"]").forEach(el => el.addEventListener("click", e => {
-//   const canInsert = checkItemAvailability({ node: document.querySelector(`.offcanvas-body`) });
-//   const { availability, message } = canInsert;
-
-//   if (availability) {
-//     if (e.currentTarget.checked) {
-//       insertInCompare(e);
-//     } else {
-//       removeFromCompare(e);
-//     }
-//   } else if (!e.currentTarget.checked) {
-//     removeFromCompare(e);
-//   } else {
-//     e.preventDefault();
-//     alert(message);
-//   }
-
-
-//   // UX 
-//   if (document.querySelector(`.offcanvas-body`).children.length > 0) {
-//     document.querySelector(`.offcanvas-start`).classList.add(`show`);
-//   } else {
-//     document.querySelector(`.offcanvas-start`).classList.remove(`show`);
-//   }
-// }));
-
-
-// const products = [
-//   { image: `resources/images/category-1.png`, title: `Nullam` },
-//   { image: `resources/images/category-2.png`, title: `Duis` },
-//   { image: `resources/images/category-3.png`, title: `Aliquam` },
-//   { image: `resources/images/category-4.png`, title: `Praesent` },
-//   { image: `resources/images/category-5.png`, title: `Quisque` },
-//   { image: `resources/images/category-6.png`, title: `Etiam` },
-//   { image: `resources/images/category-7.png`, title: `Cras` },
-//   { image: `resources/images/category-8.png`, title: `Vivamus` }
-// ];
-
-
 let compareCollection = [];
 
 const addItem = ({ name, img, id, compareCollection }) => [...compareCollection, { name, img, id }];
 
+// Checkbox elemente
 document.querySelectorAll("[data-role=\"compare-trigger\"]").forEach(el => el.addEventListener("click", e => {
 
   if (compareCollection.length === 3 && e.currentTarget.checked) {
@@ -90,54 +21,57 @@ document.querySelectorAll("[data-role=\"compare-trigger\"]").forEach(el => el.ad
     compareCollection = [...compareCollection].filter(({ id }) => id !== e.currentTarget.id);
   }
 
-  console.log(compareCollection);
+  // console.log(compareCollection);
   RenderCompareCollection({ collection: compareCollection });
   RegisterButtonEvents();
 }));
 
-
+// Buton de close pe fiecare element din Compare
 const RegisterButtonEvents = () => {
   document.querySelectorAll(".offcanvas-body .btn-close").forEach(n => {
     n.addEventListener("click", e => {
       const idSelected = e.currentTarget.closest("[data-ref]").dataset.ref;
       compareCollection = [...compareCollection].filter(({ id }) => id !== idSelected);
 
-      console.log(compareCollection);
+      // console.log(compareCollection);
       RenderCompareCollection({ collection: compareCollection });
       document.querySelector(`#${idSelected}`).checked = false;
       RegisterButtonEvents();
     });
   });
 };
+
+
 const RenderCompareCollection = ({ collection }) => {
 
   if (collection.length === 0) {
-    document.getElementById("offcanvasCompare").remove();
+    document.getElementById("offcanvasCompare").classList.remove(`show`);
     return;
   }
 
+  // console.log(collection[collection.length - 1]);
+  let lastEl = [];
+  // lastEl.push(collection[collection.length - 1]);
+  // console.log(lastEl);
 
+  const output =
+    collection.map(({ id, name, img }) => `
+        <div data-ref="${id}" class="compare-item">
+          <div class="d-flex">
+            <img src= "${img}">
+            <button type="button" class="btn-close"> </button>
+          </div >
+        <h4>${name}</h4>
+        </div >  
+        `).join("");
 
-  const output = `  
-  <div class="offcanvas offcanvas-start show" tabindex="-1" id="offcanvasCompare"  aria-labelledby="offcanvasExampleLabel">
-        <div class="offcanvas-header">
-          <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close" id="btn-close-compare"></button>
-        </div>
-        <div class="offcanvas-body">
-            ${collection.map(({id,name,img})=>`
-              <div data-ref="${id}" class="compare-item">
-                <div class="d-flex">
-                  <img src= "${img}">
-                  <button type="button" class="btn-close"> </button>
-                </div >
-              <h4>${name}</h4>
-              </div >  
-              `).join("")}
-        </div>
-  </div>
-  
-  `;
-  document.querySelector(`body`).insertAdjacentHTML("beforeend", output);
+  // Elimin toate nodurile din offcanvas-body inainte de a le adauga pe cele noi. 
+  while (document.querySelector(`.offcanvas-body`).firstChild) {
+    document.querySelector(`.offcanvas-body`).removeChild(document.querySelector(`.offcanvas-body`).firstChild);
+  }
+
+  document.querySelector(`.offcanvas-start`).classList.add(`show`);
+  document.querySelector(`.offcanvas-body`).insertAdjacentHTML("beforeend", output);
 };
 
 
